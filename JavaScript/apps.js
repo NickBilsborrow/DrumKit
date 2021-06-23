73bb32083b5026f424acfe342d6226e898b5a7fa
Nick Bilsborrow <nickbilsborrow15@gmail.com>
2021-06-23 14:38:15 +0100

    update

--- JavaScript/apps.js
+++ JavaScript/apps.js
@@ -1,3 +1,63 @@
+const SoundList = [boom,clap,hihat,kick,openhat,ride,snare,tink,tom];
+
+const playsound = (sound) => {
+   
+    let element = document.getElementById(sound)
+    element.pause();
+    element.currentTime = 0;
+    element.play();  
+       };
+
+const liveMusic = {
+    ride: [
+        "ridePic",
+        "ridePicHit"] ,
+    tom: [
+        "tomPic",
+        "tomPicHit"] ,
+    kick: [
+        "kickPic",
+        "kickPicHit"
+        ] ,    
+    snare: [
+        "snarePic",
+        "snarePicHit"] ,     
+    hihat: [
+        "hihatPic",
+        "hihatPicHit"]          
+    };
+
+// console.log(liveMusic.ride[0])
+
+
+// for (let i=0 ; i < array.length ; i++){
+// let array =[rideTimeout,tomTimeout,kickTimeout,snareTimeout,hihatTimeout];
+// let array[i] =
+// }
+
+
+
+// const timeOut=(instrument)=>{setTimeout(()=>{
+//     document.getElementById(liveMusic[instrument][1]).style.display ="none";
+//     document.getElementById(liveMusic[instrument][0]).style.display ="block"; }      
+//     ,1000);}
+
+
+
+
+const animation = async function (instrument,i) {
+    clearTimeout(i)
+    console.log(liveMusic[instrument][0]);
+    console.log(liveMusic[instrument][1]);
+    document.getElementById(liveMusic[instrument][0]).style.display ="none";
+    document.getElementById(liveMusic[instrument][1]).style.display ="block";
+
+   i = setTimeout(()=>{
+        console.log(1)
+        document.getElementById(liveMusic[instrument][1]).style.display ="none";
+        document.getElementById(liveMusic[instrument][0]).style.display ="block"; }      
+        ,500);
+}
 
 const ridePic = document.getElementById("ridePic");
 const ridePicHit = document.getElementById("ridePicHit");
@@ -10,70 +70,44 @@ document.addEventListener('keypress', (event) =>{
         console.log(letter);
         if(letter == "a"){
             console.log("A");
-            document.getElementById('boom').pause();
-            document.getElementById('boom').currentTime = 0;
-            document.getElementById('boom').play();
-            // ridePic.style.display = "none"
-            // ridePicHit.style.display = "block"
-            // setTimeout(ridePic.style.display = "block",
-            // ridePicHit.style.display = "none",
-            // 5000);                   
+            playsound("boom");
         }
-
         else if(letter == "s"){
-            console.log("S")
-            document.getElementById('clap').pause();
-            document.getElementById('clap').currentTime = 0;
-            document.getElementById('clap').play();
+            console.log("S");
+            playsound("clap");
         }
-
         else if(letter == "d"){
-            console.log("D")
-            document.getElementById('hihat').pause();
-            document.getElementById('hihat').currentTime = 0;
-            document.getElementById('hihat').play();
+            console.log("D");
+            playsound("hihat");
+            animation("hihat",1);
         }
-
         else if(letter == "f"){
-            console.log("F")
-            document.getElementById('kick').pause();
-            document.getElementById('kick').currentTime = 0;
-            document.getElementById('kick').play();
+            console.log("F");
+            playsound("kick");
+            animation("kick",2)
         }
-
         else if(letter == "g"){
-            console.log("G")
-            document.getElementById('openhat').pause();
-            document.getElementById('openhat').currentTime = 0;
-            document.getElementById('openhat').play();
+            console.log("G");
+            playsound("openhat");
         }
-
         else if(letter == "h"){
             console.log("H")
-            document.getElementById('ride').pause();
-            document.getElementById('ride').currentTime = 0;
-            document.getElementById('ride').play();
-        }
-
+            playsound("ride");
+            animation("ride",3)
+        } 
         else if(letter == "j"){
             console.log("J")
-            document.getElementById('snare').pause();
-            document.getElementById('snare').currentTime = 0;
-            document.getElementById('snare').play();
+            playsound("snare")
+            animation("snare",4)
         }
-
         else if(letter == "k"){
             console.log("K")
-            document.getElementById('tink').pause();
-            document.getElementById('tink').currentTime = 0;
-            document.getElementById('tink').play();
+            playsound("tink")
         }
-
         else if(letter == "l"){
             console.log("L")
-            document.getElementById('tom').pause();
-            document.getElementById('tom').currentTime = 0;
-            document.getElementById('tom').play();
+            playsound("tom")
+            animation("tom",5)
         }
 
 });
